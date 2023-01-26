c84875206744b06445faa366e8aa5fada5ccdea7
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-26 10:01:18 +0900

    added .env and files

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBEInQjw9cpJDyD5k38602g-eFBe2t1TfI",
-  authDomain: "react-todo-9b75a.firebaseapp.com",
-  projectId: "react-todo-9b75a",
-  storageBucket: "react-todo-9b75a.appspot.com",
-  messagingSenderId: "577432884059",
-  appId: "1:577432884059:web:ed5002050518643461407e"
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`
 };
 
 // Initialize Firebase
